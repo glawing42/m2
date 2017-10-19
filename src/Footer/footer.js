53f248216357ed361acb45861d121eebea2012e0
Graham Lawing <graham.lawing@gmail.com>
2017-10-19 17:20:53 -0400

    updated yarn lockfile

--- src/Footer/footer.js
+++ src/Footer/footer.js
@@ -35,8 +35,8 @@ class Footer extends Component {
 							<div className="col-lg-3 col-sm-3 col-xs-5">
 								<p>Connect with us! </p>
 							<i className="fa fa-linkedin fa-3x" aria-hidden="true"></i>
-							<SocialIcon url="https://www.facebook.com/m2graphicsllc/" />
-							<SocialIcon url="https://www.instagram.com/m2_graphics/" />
+							{/* <SocialIcon url="https://www.facebook.com/m2graphicsllc/" />
+							<SocialIcon url="https://www.instagram.com/m2_graphics/" /> */}
 						</div>
 						</div>
 
